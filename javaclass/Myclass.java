f9427618fa2b903a979a76aa9c89b3122969f049
gummadimahalakshmi <105544987+gummadimahalakshmi@users.noreply.github.com>
2023-03-31 20:52:49 -0500

    adding javaclass

---
+++ javaclass/Myclass.java
@@ -0,0 +1,25 @@
+package javaclass;
+
+public class Myclass {
+	
+ public static void main(String[] args) {
+
+		        System.out.println(areWePartying(true, true));
+		        System.out.println(areWePartying(true, false));
+		        System.out.println(areWePartying(false, true));
+		        System.out.println(areWePartying(false, false));
+		    }
+
+		    // return true if we are partying
+		    // we are partying if it is a weekend or it is a festival
+		    public static boolean areWePartying(boolean weekend, boolean festival) {
+		        if (weekend == true || festival == true) {
+		            return true;
+		        } else {
+		            return false;
+		        }
+		    }
+		
+	}
+
+
---
+++ javaclass/Someclass.java
@@ -0,0 +1,29 @@
+package javaclass;
+
+public class Someclass {
+
+	public static void main(String[] args) {
+		        int[] a = {4, 7, 1, 9};
+		        // The maximum index value can only be = 3
+		        // For any array of length l, max index = l-1
+		       
+
+		    // Write a method that prints your name 500 times
+		   // public static void printMyName() {
+  for (int i =0;i<a.length;i++) {
+		            	System.out.println(a[i]);
+		            }
+				       printMyName();
+				    }
+
+				   
+				 public static void printMyName() {
+
+				       for (int i=0; i<6; i++) {
+				            System.out.println("Sunil");
+				       
+		            }
+		   
+	}
+
+}
